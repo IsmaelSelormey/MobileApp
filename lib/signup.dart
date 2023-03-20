56b778018f35f34296a281d4a60c52c816d4fcd3
Julius-Abanga <juliusabanga.ja@gmail.com>
2023-03-20 18:25:21 +0000

    I created a signup page

---
+++ lib/signup.dart
@@ -0,0 +1,87 @@
+import 'package:delivery_app/home.dart';
+import 'package:delivery_app/reusablewidgets/reusablewidgets.dart';
+import 'package:delivery_app/utils/colour_utils.dart';
+import 'package:flutter/material.dart';
+
+class SignUpView extends StatefulWidget {
+  const SignUpView({Key? key}) : super(key: key);
+
+  @override
+  State<SignUpView> createState() => _SignUpViewState();
+}
+
+class _SignUpViewState extends State<SignUpView> {
+  TextEditingController _passwordTextController = TextEditingController();
+  TextEditingController _emailTextController = TextEditingController();
+  TextEditingController _userNameTextController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset : false,
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+            hexStringToColor("CB2B93"),
+            hexStringToColor("9546C4"),
+            hexStringToColor("5E61F4")
+          ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(20, 120, 20, 0),
+            child: Column(
+              children: <Widget>[
+                const SizedBox(
+                  height: 60,
+                ),
+                Align(
+            alignment: Alignment.centerLeft,
+                child: const Text(
+                  "Sign Up",
+                  style: TextStyle(color: Colors.white, fontSize: 45, fontWeight: FontWeight.w800,),
+                ),),
+                Align(
+            alignment: Alignment.centerLeft,
+                child: const Text(
+                  "Create your account",
+                  style: TextStyle(color: Colors.white70, fontSize: 16, fontWeight: FontWeight.w400),
+                ),),
+                SizedBox(
+                  height: 50,
+                ),
+                reusableTextField("Enter Username", Icons.person_outline, false,
+                    _userNameTextController),
+                const SizedBox(
+                  height: 20,
+                ),
+                reusableTextField("Enter Email Address", Icons.person_outline,
+                    false, _emailTextController),
+                const SizedBox(
+                  height: 20,
+                ),
+                reusableTextField("Enter Password", Icons.lock_outlined, true,
+                    _passwordTextController),
+                const SizedBox(
+                  height: 20,
+                ),
+                reusableTextField("Confirm Password", Icons.lock_outlined, true,
+                    _passwordTextController),
+                const SizedBox(
+                  height: 35,
+                ),
+                signInSignUpButton(context, false, () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => HomeView()));
+                })
+              ],
+            ),
+          ),
+        )
+      );
+  }
+}
