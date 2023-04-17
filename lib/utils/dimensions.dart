fd18527d1d1359e9e84f7a4c9d564b8f283abeb8
IshmaelSelormey <87906078+IsmaelSelormey@users.noreply.github.com>
2023-04-17 03:11:12 -0700

    Update dimensions.dart

--- lib/utils/dimensions.dart
+++ lib/utils/dimensions.dart
@@ -19,6 +19,7 @@ class Dimensions{
   static double Width30 =screenHeight/28.13;
 
 //font size
+  static double font16 = screenHeight/52.75;
   static double font20 =screenHeight/42.2;
   static double font26 =screenHeight/32.46;
 
@@ -29,6 +30,7 @@ class Dimensions{
 
   //icon size
 static double iconSize24 =screenHeight/35.17;
+static double iconSize16 =screenHeight/52.75;
 
 //list view size
 static double listViewImgSize = screenWidth/3.25;
