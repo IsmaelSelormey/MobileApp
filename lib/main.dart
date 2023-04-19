2ccc648d6309ff33daf117d66979a4f6b544ccb8
IshmaelSelormey <87906078+IsmaelSelormey@users.noreply.github.com>
2023-04-19 03:15:25 -0700

    Updated main classs

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:project/controllers/popular_product_controller.dart';
+import 'package:project/data/repository/popular_product_repo.dart';
+import 'package:project/pages/home/food_page_body.dart';
 import 'package:project/pages/home/main_food_page.dart';
 import 'package:get/get.dart';
 import 'package:project/onboarding_screen.dart';
@@ -19,6 +22,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+      Get.find<PopularProductController>().getPopularProductList();
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Ecommence App',
@@ -26,7 +30,7 @@ class MyApp extends StatelessWidget {
 
         primarySwatch: Colors.brown,
       ),
-      home: RecommendedFoodDetail(),
+      home:MainFoodPage(),
     );
   }
 }
