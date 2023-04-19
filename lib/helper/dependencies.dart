83feaeced5c625bf7cb0a965e1a34bf855464b0b
IshmaelSelormey <87906078+IsmaelSelormey@users.noreply.github.com>
2023-04-19 03:15:06 -0700

    Updated dependencies

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -1,10 +1,12 @@
 import 'package:project/controllers/popular_product_controller.dart';
-import 'package:project/data/api/api_class.dart';
+import 'package:project/data/api/api_client.dart';
 import 'package:get/get.dart';
 import 'package:project/data/repository/popular_product_repo.dart';
+
+import '../utils/app_constants.dart';
 Future<void> init()async{
   //api client
-  Get.lazyPut(()=>ApiClient(appBaseUrl: "appBaseUrl"));
+  Get.lazyPut(()=>ApiClient(appBaseUrl: AppConstants.BASE_URL));
 
   //repos
   Get.lazyPut(() => PopularProductRepo(apiClient: Get.find()));
