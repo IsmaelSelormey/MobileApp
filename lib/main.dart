8c2375fabc44eec0702c265a345a17ffb9ad6883
IshmaelSelormey <87906078+IsmaelSelormey@users.noreply.github.com>
2023-04-19 01:00:14 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:project/home/main_food_page.dart';
+import 'package:project/pages/home/main_food_page.dart';
 import 'package:get/get.dart';
 import 'package:project/onboarding_screen.dart';
 import 'package:project/pages/home/food/popular_food_detail.dart';
 import 'package:project/pages/home/food/recommended_food_detail.dart';
 import 'package:project/signin.dart';
 import 'package:project/signup.dart';
-void main() {
+import 'helper/dependencies.dart' as dep;
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dep.init();
   runApp(const MyApp());
 }
 
