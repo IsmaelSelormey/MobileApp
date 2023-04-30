ef21f4bdba4d8af621a5ac48dab1318a4f47fe4c
IshmaelSelormey <87906078+IsmaelSelormey@users.noreply.github.com>
2023-04-30 13:26:53 -0700

    Update dependencies.dart

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -3,14 +3,18 @@ import 'package:project/data/api/api_client.dart';
 import 'package:get/get.dart';
 import 'package:project/data/repository/popular_product_repo.dart';
 
+import '../controllers/recommended_product_controller.dart';
+import '../data/repository/recommended_product_repo.dart';
 import '../utils/app_constants.dart';
 Future<void> init()async{
   //api client
   Get.lazyPut(()=>ApiClient(appBaseUrl: AppConstants.BASE_URL));
 
   //repos
-  Get.lazyPut(() => PopularProductRepo(apiClient: Get.find()));
+  Get.lazyPut(() => PopularProductRepo(apiClient:Get.find()));
+  Get.lazyPut(() => RecommendedProductRepo(apiClient: Get.find()));
   
   //controllers
-  Get.lazyPut(() => PopularProductController(popularProductRepo: Get.find()));
+  Get.lazyPut(() => PopularProductController(popularProductRepo:Get.find()));
+  Get.lazyPut(() => RecommendedProductController(recommendedProductRepo: Get.find()));
 }
